47178a007414ce0af402b78265861075fbcd34cd
yuanyuanyao <525784669@qq.com>
2022-04-12 23:26:55 +0200

    update test files

---
+++ src/test_custom.py
@@ -0,0 +1,25 @@
+import os
+import os.path
+import numpy as np
+import pickle
+from src.utils.nnsearch import *
+from src.utils.evaluate import mAP_custom
+
+def load_path_features(dataset):
+    if '/' in dataset:
+        dataset = dataset.replace('/', '_')
+    file_path_feature = 'outputs/' + dataset + '_path_feature.pkl'
+    with open(file_path_feature, 'rb') as pickle_file:
+        path_feature = pickle.load(pickle_file)
+    vecs = path_feature['feature']
+    img_r_path = path_feature['path']
+    return vecs, img_r_path
+
+Custom_q, relpaths_q = load_path_features('Custom/query')
+Custom_d, relpaths_d = load_path_features('Custom/database')
+
+n_database = Custom_d.shape[1]
+K = n_database
+match_idx, time_per_query = matching_L2(K, Custom_d.T, Custom_q.T)
+mAP = mAP_custom(K, match_idx, relpaths_q, relpaths_d)
+print('mean average precision: ', mAP)
